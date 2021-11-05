2936cdd6a399e695937073298c1759c8aeb482ca
Firdausa <firdausa@smktelkom-mlg.sch.id>
2021-11-05 08:56:33 +0700

    add JWT middleware

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,10 @@ use App\Http\Controllers\OutletController;
 Route::post('register', [UserController::class,'register']);
 Route::post('login', [UserController::class, 'login']);
 
-Route::post('outlet', [OutletController::class, 'insert']);
-Route::put('outlet/{id}', [OutletController::class, 'update']);
-Route::delete('outlet/{id}', [OutletController::class, 'delete']);
-Route::get('outlet', [OutletController::class, 'getAll']); //get all
-Route::get('outlet/{id_outlet}', [OutletController::class, 'getById']); //get all
+Route::group(['middleware' => ['jwt.verify:admin']], function () {
+    Route::post('outlet', [OutletController::class, 'insert']);
+    Route::put('outlet/{id}', [OutletController::class, 'update']);
+    Route::delete('outlet/{id}', [OutletController::class, 'delete']);
+    Route::get('outlet', [OutletController::class, 'getAll']); //get all
+    Route::get('outlet/{id_outlet}', [OutletController::class, 'getById']); //get all
+});
